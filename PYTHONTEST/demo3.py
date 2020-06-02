17bcd1bc174f6d99b06c077e1a27bb77ad5efeee
Superf <296769530@qq.com>
2020-06-02 22:08:04 +0800

    更新2

--- PYTHONTEST/demo3.py
+++ PYTHONTEST/demo3.py
@@ -1,16 +1,40 @@
 
 
+# db = [
+#         {"username":"张三","password":"123456"},
+#         {"username":"李四","password":"123456789"}
+#     ]
+
+# zh = input("请输入账号:")
+# mm = input("请输入密码:")
+
+# for i in db:
+#     if zh == i.get("username"):
+#         if mm == i.get("password"):
+#             print("{}登录成功！".format(zh))
+#             break
+#     else:
+#         print("登录失败！")
+#     continue    
+
 db = [
         {"username":"张三","password":"123456"},
         {"username":"李四","password":"123456789"}
     ]
 
-zh = input("请输入账号:")
-mm = input("请输入密码:")
+zh = input("请输入账号：")
+pd = input("请输入密码：")
 
+count = 1
 for i in db:
-    print("=======================")
-    if zh == i.get("username") and mm == i.get("password"):
-        print("输入的账号{}登录成功".format(zh))
+    if zh == i.get("username"):
+        i["password"] = pd
+        break
     else:
-        print("登陆失败！")    
+        if len(db) == count:
+            db.append({"username":zh,"password":pd})
+    count = count + 1    
+print(db)    
+     
+
+            
